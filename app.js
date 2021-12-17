2f9b5653235dc757fb5d98a86b60845bdda54e5f
donuts030 <ye.danning@gmail.com>
2021-12-17 16:27:22 +0800

    made a moving rectangle

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
          
-var c=document.getElementById("playScreen");
-var ctx=c.getContext("2d");
-ctx.font="30px Georgia";
-
-ctx.fillRect(0, 0, 20, 10);
+const canvas=document.getElementById("playScreen");
+const ctx=canvas.getContext("2d");
+let x = canvas.width/2;
+let y = canvas.height-30;
+let dx = -2;
+let dy = 0;
 
 function draw(){
-
+    player();
 }
+setInterval(draw, 10);
 
 function player(){
-    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.fillRect(x, y, 20, 10);
+    //ctx.fillStyle = 'rgb(' + parseInt(Math.random() * 255) + ',' + parseInt(Math.random() * 255) + ',' + parseInt(Math.random() * 255) + ')'
+    ctx.closePath();
+    x += dx;
+    y += dy;
+}
+
+function playerMovement(){
+
 }
