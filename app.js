890c3acd13d99f90b13e0ff960459b14383507d1
donuts030 <ye.danning@gmail.com>
2021-12-17 15:13:49 +0800

    project files added

---
+++ app.js
@@ -0,0 +1,14 @@
+         
+var c=document.getElementById("playScreen");
+var ctx=c.getContext("2d");
+ctx.font="30px Georgia";
+
+ctx.fillRect(0, 0, 20, 10);
+
+function draw(){
+
+}
+
+function player(){
+    
+}
