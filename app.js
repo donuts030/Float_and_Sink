006fff90dae3f58328b82c6fc00538cc1de9b362
donuts030 <ye.danning@gmail.com>
2021-12-21 20:57:26 +0800

    know what blocks are collided into

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const ctx=canvas.getContext("2d");
 let rightPressed = false;
 let leftPressed = false;
 let spacePressed = false;
-
+const xSpeed = 2;
+const ySpeed = 1;
 const baseVar ={
-    x: 0,
-    y: 0,
-    dx: 2,
-    dy: -1,
+    x: 1,
+    y: 1,
 
+    dy: ySpeed,
+    
     sink: true,
+
+    //number of cells in a row and in a column: can change to 2 diff values later
+    cellNum: 10,
 
     grid: [ [2, 0, 1, 1, 1, 0, 1, 1, 1, 0],
             [1, 0, 0, 0, 1, 1, 1, 0, 1, 0],
@@ -48,21 +52,38 @@ function playerCtrlPressed(e){
         leftPressed = true;
     }
     else if(e.keyCode === 32) { //left
+        console.log("pressed space");
         spacePressed = true;
     }
 }
 
 function playerCtrlReleased(e){
     console.log(e);
-    if(e.keyCode === 39) {
-        rightPressed = false;
-    }
-    else if(e.keyCode === 37) {
-        leftPressed = false;
-    }
+    // if(e.keyCode === 39) {
+    //     rightPressed = false;
+    // }
+    // else if(e.keyCode === 37) {
+    //     leftPressed = false;
+    // }
 /*     else if(e.keyCode === 32) { //left
         spacePressed = false;
     } */
+}
+
+
+function createMaze(grid){
+    ctx.beginPath();
+    //number of cells in a row and in a column: can change to 2 diff values later
+    //const cellNum = 10 
+    for(let x = 0; x < baseVar.cellNum; x++){
+        for(let y = 0; y < baseVar.cellNum; y++){
+            let cellType = grid[y][x];
+            createBlock(baseVar.cellNum, x, y, cellType);
+        }
+    }
+    
+    ctx.closePath();
+    
 }
 
 function createBlock(cellNum, x, y, cellType){
@@ -86,62 +107,162 @@ function createBlock(cellNum, x, y, cellType){
     //ctx.fillStyle = 'rgb(' + parseInt(Math.random() * 255) + ',' + parseInt(Math.random() * 255) + ',' + parseInt(Math.random() * 255) + ')'
 }
 
-function createMaze(grid){
-    ctx.beginPath();
-    //number of cells in a row and in a column: can change to 2 diff values later
-    const cellNum = 10 
-    for(let x = 0; x < cellNum; x++){
-        for(let y = 0; y < cellNum; y++){
-            let cellType = grid[y][x];
-            createBlock(cellNum, x, y, cellType);
-        }
-    }
-    
-    ctx.closePath();
-    
 
-}
 
 function player(baseVar){
     const cellNum = 10
     const playerSize = canvas.width / cellNum - 1;
-    let x = baseVar.x;
-    let y = baseVar.y;
-    let dx = baseVar.dx;
-    let dy = baseVar.dy;
     ctx.beginPath();
     ctx.fillStyle = 'rgb(0, 255, 0)'
-    ctx.fillRect(x, y, playerSize, playerSize);
+    ctx.fillRect(baseVar.x, baseVar.y, playerSize, playerSize);
     //ctx.fillStyle = 'rgb(' + parseInt(Math.random() * 255) + ',' + parseInt(Math.random() * 255) + ',' + parseInt(Math.random() * 255) + ')'
     ctx.closePath();
-
     //player movement 
-    if(y + dy < 0) {
-        dy = 0;
-    }
-    if(y + dy > (canvas.height - playerSize)) {
-        dy = 0;
-    }
-
-    if (rightPressed) {
-       baseVar.x += dx; 
-    }
-    else if(leftPressed){
-        baseVar.x -= dx;
-    }
-    else if (spacePressed){
-
-        baseVar.dy = -baseVar.dy;
-        console.log(baseVar.dy);
-        spacePressed = false;
-    }
-    baseVar.y += dy;
-
-
-
+    playerMovement(playerSize);
     
 }
 
-function playerMovement(){
+function collisionDetection(){
+    const blockLength = canvas.width / baseVar.cellNum;// combine later
+    const playerWidth = blockLength - 1;
+    const playerHeight = playerWidth;
+    for(let col = 0; col < baseVar.cellNum; col++){
+        for(let row = 0; row < baseVar.cellNum; row++){
+            if (baseVar.grid[row][col] === 0) {
+                //check if collided with player
+                let gridX = col * blockLength;
+                let gridY = row * blockLength;
+                // if (row == 9)
+                //     console.log ("gridX:" + gridX + " playX:" + baseVar.x);
+                if ((Math.abs(gridX - baseVar.x) < playerWidth) 
+                    && (Math.abs(gridY - baseVar.y) < playerHeight )){
+                
+                    console.log("collided at :" + row + ", "+ col);
 
+                }
+
+            }
+        }
+    }
+}
+
+
+function playerMovement(playerSize){
+
+    let dx = xSpeed;
+    let dy = baseVar.dy;
+    let tempPlayerX = baseVar.x;
+    let tempPlayerY = baseVar.y;
+
+    const blockWidth = canvas.width / baseVar.cellNum;
+    let col = Math.floor(baseVar.x/ blockWidth);
+    let row = Math.floor(baseVar.y / blockWidth);
+
+
+    if (rightPressed) {
+        if((baseVar.x + dx + playerSize) > canvas.width){
+            dx = 0;
+        }
+        if(col >= (baseVar.cellNum - 1)){
+            dx = 0
+        }
+
+        baseVar.x += dx; 
+        rightPressed = false;
+    }
+    else if(leftPressed){
+        if(baseVar.x <= 0){
+            dx = 0;
+        }
+        if(col < 0 ){
+            dx = 0
+        }
+        // if (baseVar.grid[row][col])
+        baseVar.x -= dx;
+        leftPressed= false;
+    }
+
+    else if (spacePressed){
+
+        baseVar.dy = -baseVar.dy;
+        spacePressed = false;
+
+    }    
+
+    if(baseVar.dy > 0){
+        if(row >= (baseVar.cellNum - 1)){
+            dy = 0;
+        }
+    }
+    else{
+        if(baseVar.y <= 0){
+            dy = 0;
+        }
+    }
+    baseVar.y += dy;
+    
+    collisionDetection();
+        
+       /*  if (baseVar.dy === 0) { // pos y not move
+            let col = Math.floor((baseVar.y) / blockWidth); // find curr row num
+            let row = Math.floor(baseVar.x / blockWidth); // find curr col num
+            if (col > 0 && baseVar.grid[col-1][row] !== 0) { // block above not wall 
+                baseVar.dy = -1; 
+            }
+            else if (col < 9 && baseVar.grid[col+1][row] !== 0) {
+                baseVar.dy = 1;
+            }
+        }
+        else {
+            baseVar.dy = -baseVar.dy;
+            console.log(baseVar.dy);
+        }
+        spacePressed = false;
+        console.log("pressed space dy=" +  baseVar.dy);
+    }
+
+    if (baseVar.dy > 0) {
+        if((baseVar.y + playerSize) > canvas.height){
+            baseVar.dy = 0;
+        }
+
+        let row = Math.floor(baseVar.x / blockWidth);
+        let col = Math.floor((baseVar.y + playerSize) / blockWidth);
+        
+        if(baseVar.grid[col][row] === 0){
+            baseVar.dy = 0
+        }
+
+        baseVar.y += baseVar.dy; 
+    }
+    else if (baseVar.dy < 0) {
+        if(baseVar.y <= 0){
+            baseVar.dy = 0;
+        }
+
+        let row = Math.floor(baseVar.x / blockWidth);
+        let col = Math.floor(baseVar.y / blockWidth);
+
+        if(baseVar.grid[col][row] === 0){
+            baseVar.dy = 0
+        }
+
+        baseVar.y += baseVar.dy;
+    }
+     */
+    // let colNum = Math.floor(baseVar.x / blockWidth);
+    // let rowNum = Math.floor(baseVar.y / blockWidth);
+
+    // if((baseVar.y + baseVar.dy) >= 0 && (baseVar.y + baseVar.dy) < (canvas.height - playerSize)){
+
+    //     baseVar.y += baseVar.dy;
+    // }    
+
+
+}
+
+function blockCollision(){
+    //use baseVar.grid to determine area of collision
+
+    
 }
