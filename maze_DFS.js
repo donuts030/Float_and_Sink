d4181314b548eda31f0ece4361660bafa8344ef0
donuts030 <ye.danning@gmail.com>
2022-01-04 14:53:58 +0800

    add from original

--- maze_DFS.js
+++ maze_DFS.js
@@ -3,9 +3,9 @@ let mazeSize;
 
 let grid;
 
-let visited ;
+let visited;
 
-let stack = [{currRow: 0, currCol: 0}];
+let stack = [{ currRow: 0, currCol: 0 }];
 
 const longestStack = {
     length: 0,
@@ -17,11 +17,11 @@ let currRow = 0;
 let currCol = 0;
 
 
-function mazeGenerator(cellNum){
+function mazeGenerator(cellNum) {
     mazeSize = cellNum;
     grid = [];
     visited = [];
-    stack = [{currRow: 0, currCol: 0}];
+    stack = [{ currRow: 0, currCol: 0 }];
     longestStack.length = 0;
     longestStack.row = 0;
     longestStack.col = 0;
@@ -30,20 +30,20 @@ function mazeGenerator(cellNum){
     startGrid();
     createVisitedList();
     mazeSearch();
-    for(let i = 0; i < mazeSize; i++){
-    console.log(grid[i].toString()); 
+    for (let i = 0; i < mazeSize; i++) {
+        console.log(grid[i].toString());
     }
     return grid;
 }
 
-function startGrid(){
-    for(let row = 0; row < mazeSize; row++){
+function startGrid() {
+    for (let row = 0; row < mazeSize; row++) {
         let arrRow = [];
-        for(let col = 0; col < mazeSize; col++){
-            if(row % 2 !== 0 || col % 2 !== 0){
+        for (let col = 0; col < mazeSize; col++) {
+            if (row % 2 !== 0 || col % 2 !== 0) {
                 arrRow[col] = 0;
             }
-            else{
+            else {
                 arrRow[col] = 1;
             }
         }
@@ -55,53 +55,53 @@ function startGrid(){
     console.log(grid);
 }
 
-function mazeSearch(){
-    console.log("stack length: "+stack.length);
-    let direction = Math.floor(Math.random() * 4) ;// 0 is top, 1 is right, 2 is bottom, 3 is left
+function mazeSearch() {
+    console.log("stack length: " + stack.length);
+    let direction = Math.floor(Math.random() * 4);// 0 is top, 1 is right, 2 is bottom, 3 is left
     let init = direction;
     let add = Math.floor(Math.random() * 2);
-    // const top = grid[currRow][currCol - 2];
-    // const right = grid[currRow + 2][currCol];
-    // const bottom = grid[currRow][currCol + 2];
-    // const left = grid[currRow - 2][currCol];
-    while(stack.length > 0){
+    const top = 0;
+    const right = 1;
+    const bottom = 2;
+    const left = 3;
+    while (stack.length > 0) {
 
-        if((currCol - 2)>= 0 && direction === 0 && visited[currRow][currCol - 2] === 0){
+        if ((currCol - 2) >= 0 && direction === top && visited[currRow][currCol - 2] === 0) {
             grid[currRow][currCol - 1] = 1;
             visited[currRow][currCol - 2] = 1;
             currCol -= 2;
-            stack.push({currRow, currCol});
+            stack.push({ currRow, currCol });
             direction = Math.floor(Math.random() * 4);
             init = direction;
             add = Math.floor(Math.random() * 2);
             checkLongest()
 
         }
-        else if((currRow + 2) < mazeSize  && direction === 1 && visited[currRow + 2][currCol] === 0 ){
+        else if ((currRow + 2) < mazeSize && direction === right && visited[currRow + 2][currCol] === 0) {
             grid[currRow + 1][currCol] = 1;
             visited[currRow + 2][currCol] = 1;
             currRow += 2;
-            stack.push({currRow, currCol});
+            stack.push({ currRow, currCol });
             direction = Math.floor(Math.random() * 4);
             init = direction;
             add = Math.floor(Math.random() * 2);
             checkLongest()
         }
-        else if((currCol + 2) < mazeSize && direction === 2 && visited[currRow][currCol + 2] === 0){
+        else if ((currCol + 2) < mazeSize && direction === bottom && visited[currRow][currCol + 2] === 0) {
             grid[currRow][currCol + 1] = 1;
             visited[currRow][currCol + 2] = 1;
             currCol += 2;
-            stack.push({currRow, currCol});
+            stack.push({ currRow, currCol });
             direction = Math.floor(Math.random() * 4);
             init = direction;
             add = Math.floor(Math.random() * 2);
             checkLongest()
         }
-        else if((currRow - 2)>= 0 && direction === 3 && visited[currRow - 2][currCol] === 0 ){
+        else if ((currRow - 2) >= 0 && direction === left && visited[currRow - 2][currCol] === 0) {
             grid[currRow - 1][currCol] = 1;
             visited[currRow - 2][currCol] = 1;
             currRow -= 2;
-            stack.push({currRow, currCol});
+            stack.push({ currRow, currCol });
             direction = Math.floor(Math.random() * 4);
             init = direction;
             add = Math.floor(Math.random() * 2);
@@ -110,19 +110,19 @@ function mazeSearch(){
 
         else {
 
-            if (add === 0){
-                if( ++ direction >= 4){
-                    direction = 0;
+            if (add === 0) {
+                if (++direction > left) {
+                    direction = top;
                 }
             }
 
-            else{
-                if( -- direction < 0){
-                    direction = 3;
+            else {
+                if (--direction < top) {
+                    direction = left;
                 }
-            } 
-            
-            if(direction === init){
+            }
+
+            if (direction === init) {
                 backtrack();
                 direction = Math.floor(Math.random() * 4);
                 init = direction;
@@ -130,22 +130,22 @@ function mazeSearch(){
             }
 
         }
-        
+
     }
     grid[longestStack.row][longestStack.col] = 3; //set end cell
 
 
 }
 
-function createVisitedList(){
+function createVisitedList() { //create a similar grid just to store whether the maze is visited
 
-    for(let row = 0; row < mazeSize; row++){
+    for (let row = 0; row < mazeSize; row++) {
         let arrRow = [];
-        for(let col = 0; col < mazeSize; col++){
-            if(row % 2 !== 0 || col % 2 !== 0){
+        for (let col = 0; col < mazeSize; col++) {
+            if (row % 2 !== 0 || col % 2 !== 0) {
                 arrRow[col] = 2;
             }
-            else{
+            else {
                 arrRow[col] = 0;
             }
         }
@@ -157,14 +157,14 @@ function createVisitedList(){
     //console.log(visited);
 }
 
-function backtrack(){
+function backtrack() {
     const currCell = stack.pop();
     currRow = currCell["currRow"];
     currCol = currCell["currCol"];
 }
 
-function checkLongest(){
-    if(longestStack.length < stack.length){
+function checkLongest() {
+    if (longestStack.length < stack.length) {
         longestStack.length = stack.length;
         longestStack.row = currRow;
         longestStack.col = currCol;
